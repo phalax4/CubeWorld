059a05130984100b0eee34899e40c4361cb704ed
lucastep13 <sclucas13@gmail.com>
2017-11-20 13:58:31 -0500

    Triangles, Death, and Triangles Shooters
    -Added Triangle Minions
    
    -Added trigger to triangles to cause death of player (This needs to be
    improved later)
    
    -Added Triangle Shooters, which shoot a projectile made out of 2
    Triangle Minions

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	public float destroyTime;
+	// Use this for initialization
+	void Start () {
+
+		Destroy (this.gameObject, destroyTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(){
+		Destroy (this.gameObject);
+	}
+}
