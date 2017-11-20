a269d8f54264aad7838ff5419d2416b61ce26a1c
phalax4 <jeremylim995@gmail.com>
2017-11-19 21:55:38 -0600

    Testing jumpforce value

--- Assets/jump.cs
+++ Assets/jump.cs
@@ -1,22 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class jump : MonoBehaviour {
+public class jump : MonoBehaviour
+{
 
 	public float fallMultiplier = 2.5f;
-	public float lowJumpMulitplier = 2f;
+	public float lowJumpMulitplier = 100f;
 
 	Rigidbody2D rb;
 
-	void Awake(){
+	void Awake ()
+	{
 		rb = GetComponent<Rigidbody2D> ();
 	}
 
-	void Update () {
+	void Update ()
+	{
 		if (rb.velocity.y < 0) {
 			
 			rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-		} else if(rb.velocity.y > 0 && !Input.GetKey(KeyCode.W)) {
+		} else if (rb.velocity.y > 0 && !Input.GetKey (KeyCode.W)) {
 			rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMulitplier - 1) * Time.deltaTime;
 		}
 	}
