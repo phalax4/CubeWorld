0465395688dd2f0cbb38914a3d6f053031fbb635
phalax4 <jeremylim995@gmail.com>
2017-11-19 22:02:18 -0600

    Changed the right jumpforce this time

--- Assets/playercontroller.cs
+++ Assets/playercontroller.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playercontroller : MonoBehaviour {
+public class playercontroller : MonoBehaviour
+{
 	public float speedForce = 50f;
-	public float jumpForce = 5f;
+	public float jumpForce = 200f;
 
 	public Vector2 jumpVector;
 	public bool isGrounded;
@@ -13,33 +14,33 @@ public class playercontroller : MonoBehaviour {
 	public LayerMask ground;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate ()
+	{
 	
 
 		if (Input.GetKey (KeyCode.A)) {
 			
-			GetComponent<Rigidbody2D>().velocity = new Vector2 (-speedForce, GetComponent<Rigidbody2D>().velocity.y);
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-speedForce, GetComponent<Rigidbody2D> ().velocity.y);
 			transform.localScale = new Vector3 (-1, 1, 1);
 
 		} else if (Input.GetKey (KeyCode.D)) {
 			
-			GetComponent<Rigidbody2D>().velocity = new Vector2 (speedForce, GetComponent<Rigidbody2D>().velocity.y);
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (speedForce, GetComponent<Rigidbody2D> ().velocity.y);
 			transform.localScale = new Vector3 (1, 1, 1);
-		}
-		else
-			GetComponent<Rigidbody2D>().velocity = new Vector2 (0, GetComponent<Rigidbody2D>().velocity.y);
+		} else
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, GetComponent<Rigidbody2D> ().velocity.y);
 
 		isGrounded = Physics2D.OverlapCircle (grounder.transform.position, radius, ground);
 
 
-		if(Input.GetKey(KeyCode.W) && isGrounded == true) {	
-			GetComponent<Rigidbody2D> ().velocity = new Vector2
-				(GetComponent<Rigidbody2D>().velocity.x, jumpForce);
+		if (Input.GetKey (KeyCode.W) && isGrounded == true) {	
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpForce);
 		}
 
 	}
